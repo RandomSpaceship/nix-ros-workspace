bb627a515c5ac5de777c5c81aaa991d75e3d073a
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-11-28 19:39:36 +1000

    Rename overlay arguments to use final/prev instead of self/super

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,9 +1,9 @@
-self: super: {
+final: prev: {
   rosPackages = builtins.mapAttrs (
     rosDistro: rosDistroPackages:
     if rosDistroPackages ? overrideScope then
-      rosDistroPackages.overrideScope (import ./ros-distro-overlay.nix self super)
+      rosDistroPackages.overrideScope (import ./ros-distro-overlay.nix final prev)
     else
       rosDistroPackages
-  ) super.rosPackages;
+  ) prev.rosPackages;
 }
